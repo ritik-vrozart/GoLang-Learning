e242767e44285ae6c3e0e8a7c8b83ecf9ccbc7a5
Ritik Pathak <ritik@vrozart.com>
2025-12-11 23:40:49 +0530

    for-loop

--- array/main.go
+++ array/main.go
@@ -31,26 +31,53 @@ func main(){
 
 	// *******SWITCH STATEMENT*****
 
-	day := "Mondaydwd"
+	// day := "Mondaydwd"
 
-	switch day {
-		case "Monday":
-			fmt.Println("Today is Monday")
-		case "Tuesday":
-			fmt.Println("Today is Tuesday")
-		case "Wednesday":
-			fmt.Println("Today is Wednesday")
-		case "Thursday":
-			fmt.Println("Today is Thursday")
-		case "Friday":
-			fmt.Println("Today is Friday")
-		case "Saturday":
-			fmt.Println("Today is Saturday")
-		case "Sunday":
-			fmt.Println("Today is Sunday")
-		default:
-			fmt.Println("Invalid day")
-	}
+	// switch day {
+	// 	case "Monday":
+	// 		fmt.Println("Today is Monday")
+	// 	case "Tuesday":
+	// 		fmt.Println("Today is Tuesday")
+	// 	case "Wednesday":
+	// 		fmt.Println("Today is Wednesday")
+	// 	case "Thursday":
+	// 		fmt.Println("Today is Thursday")
+	// 	case "Friday":
+	// 		fmt.Println("Today is Friday")
+	// 	case "Saturday":
+	// 		fmt.Println("Today is Saturday")
+	// 	case "Sunday":
+	// 		fmt.Println("Today is Sunday")
+	// 	default:
+	// 		fmt.Println("Invalid day")
+	// }
 
+
+	// *******FOR LOOP*****
+
+	// 1 => BASIC FOR LOOP
+	// for i:=0 ; i<10; i++{
+	// 	fmt.Println("The value of i is ", i)
+	// }
+
+	// 2 => Infinite loop with break statement
+
+	// counter := 3
+
+	// for {
+	// 	fmt.Println("Counter is ", counter)
+	// 	counter++
+	// 	if counter > 10 {
+	// 		break
+	// 	}
+	// }
+
+	// 3 => For loop with range
+
+	// numbers := []int{1,2,3,4,5,6,7,8,9,10}
+
+	// for index, value := range numbers {
+	// 	fmt.Println("The value of the index is ", index, " and the value is ", value)
+	// }
 
 }
